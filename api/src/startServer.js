e52a437eb86c3fb66d767f92007b06a10f24c57c
vitorserrano <vitorcirinoserrano@gmail.com>
2020-10-07 17:57:38 -0300

    ⚡ Finalized API

---
+++ api/src/startServer.js
@@ -0,0 +1,19 @@
+import { ApolloServer, PubSub } from 'apollo-server';
+
+import mongoose from 'mongoose';
+
+const startServer = ({ typeDefs, resolvers }) => {
+  mongoose.connect('mongodb://localhost:27017/graphql', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  const pubsub = new PubSub();
+  const server = new ApolloServer({ typeDefs, resolvers, context: { pubsub } });
+
+  server
+    .listen()
+    .then(({ url }) => console.log(`🤘  Server started at ${url}`));
+};
+
+export default startServer;
